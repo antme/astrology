ba0420839e7262ef36900a990f2b0f786eaf8636
dylan <251148471@qq.com>
2017-09-03 23:08:02 +0800

    backup index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$url = $_SERVER['REQUEST_URI'];
+
+if($url=='/'){
+    header('Location:/astrology_mobile/index.html');
+}else{
+    $response->send();
+    $kernel->terminate($request, $response);
+}
