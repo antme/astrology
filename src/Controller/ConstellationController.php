b4e88f3b7c7ba3e1e5265c7d815c8e92d980e43e
dylan <251148471@qq.com>
2017-09-04 14:01:16 +0800

    add new interface

--- src/Controller/ConstellationController.php
+++ src/Controller/ConstellationController.php
@@ -34,24 +34,35 @@ class ConstellationController extends ControllerBase {
   
   public function listConstellation(){
       
-      $query = \Drupal::database()->select('node', 'n');
-      $query->fields('n', ['nid','uuid']);
-      $query->range(0, 1);
-      $results = $query->execute()->fetchAssoc();
+      $query_str = "select n.entity_id from node__field_xingzuo_name as n where n.field_xingzuo_name_value='" + $xingzuo_name  + "';";
+      $results = \Drupal::database()->query($query_str)->fetchAll();
       
       return   $results;
   }
   
   public function getConstellationInfo(){
-      $nid = $_REQUEST['nid'];
-      $query = \Drupal::database()->select('node', 'n');
-      $query->fields('n', ['nid','uuid']);
-      $query->condition('n.nid', $nid);
-      $query->range(0, 1);
-      $results = $query->execute()->fetchAssoc();
+      $xingzuo_name = $_REQUEST['xingzuo_name'];
+      $query_str = "select n.entity_id from node__field_xingzuo_name as n where n.field_xingzuo_name_value='".$xingzuo_name."';";
+      $results = \Drupal::database()->query($query_str)->fetchAll();
+      foreach($results as $key=>$value) {
+          
+          $tags_Query = "select field_xingzuobiaoqian_value as tags from node__field_xingzuobiaoqian where entity_id='" . $value->entity_id ."'";
+          $tags_results = \Drupal::database()->query($tags_Query)->fetchAll();
+          $results[$key]->tags =  $tags_results;
+          
+          $story_Query = "select field_xingzuogushi_value as stories from node__field_xingzuogushi where entity_id='" . $value->entity_id ."'";
+          $story_results = \Drupal::database()->query($story_Query)->fetchAll();
+          $results[$key]->stories =  $story_results;
+          
+          $stars_Query = "select field_xingzuomingxing_value as stars from node__field_xingzuomingxing where entity_id='" . $value->entity_id ."'";
+          $stars_results = \Drupal::database()->query($stars_Query)->fetchAll();
+          $results[$key]->stars =  $stars_results;
+          
+          
+      }
+      
       
       return   $results;
-      
   }
 
 
