08ce1cc011c145ed147c863611cd8537f25f6af4
dylan <251148471@qq.com>
2017-09-12 23:37:57 +0800

    page redirect

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -2,9 +2,8 @@
 namespace Drupal\astrology\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\astrology\service\WeixinService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class RedirectController extends ControllerBase
 {
@@ -18,8 +17,8 @@ class RedirectController extends ControllerBase
         WeixinService::authorization_code();
         
         if ($redirect_url == "") {
-            $redirect_url = "http://test.vlvlife.com";
+            $redirect_url = "http://test.vlvlife.com/#ast_redirect";
         }
-        return new RedirectResponse($redirect_url);
+        return new RedirectResponse($redirect_url+"#ast_redirect");
     }
 }
