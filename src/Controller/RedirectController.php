553b9ccde868bcd9bd0f7ac9270fc8579ed8fab8
dylan <251148471@qq.com>
2017-09-12 23:33:38 +0800

    deal with empty redirect

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -1,34 +1,25 @@
 <?php
-
 namespace Drupal\astrology\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\astrology\service\WeixinService;
 
+class RedirectController extends ControllerBase
+{
 
-class RedirectController extends ControllerBase {
-    
-    
-    public function do_req($method) {
+    public function do_req($method)
+    {
+        $redirect_url = $_COOKIE["ast_redirect_url"];
         
-        $redirect_url=$_COOKIE["ast_redirect_url"];
-        
-        
-//      $redirect_url = $_REQUEST['ast_redirect_url'];
         $code = $_REQUEST['code'];
         
         WeixinService::authorization_code();
         
-
-        
-//         $res =  new JsonResponse( $results );
-//         $res->setCallback($_REQUEST['callback']);
-//         return $res;
-        
-         return new RedirectResponse($redirect_url);
-        
+        if ($redirect_url == "") {
+            $redirect_url = "http://test.vlvlife.com";
+        }
+        return new RedirectResponse($redirect_url);
     }
-    
-    
 }
