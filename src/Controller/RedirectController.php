a886fe5245489f209c4df281e80991cf7e05bb87
dylan <251148471@qq.com>
2017-09-12 23:05:44 +0800

    for debug

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\astrology\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\astrology\service\WeixinService;
 
 
 class RedirectController extends ControllerBase {
@@ -10,11 +12,19 @@ class RedirectController extends ControllerBase {
     
     public function do_req($method) {
         
-        $redirect_url = $_REQUEST['ast_redirect_url'];
-        $code = $_REQUEST['code'];
-    
+        $redirect_url=$_COOKIE["ast_redirect_url"];
         
-        return new RedirectResponse($redirect_url);
+        
+//         $redirect_url = $_REQUEST['ast_redirect_url'];
+        $code = $_REQUEST['code'];
+        
+        $results = WeixinService::authorization_code();
+        
+        $res =  new JsonResponse( $results );
+        $res->setCallback($_REQUEST['callback']);
+        return $res;
+        
+//         return new RedirectResponse($redirect_url);
         
     }
     
