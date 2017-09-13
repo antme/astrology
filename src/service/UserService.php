394d3a24defa3004ba98c2440714a402531e2f2e
dylan <251148471@qq.com>
2017-09-13 12:04:52 +0800

    page update

--- src/service/UserService.php
+++ src/service/UserService.php
@@ -29,8 +29,13 @@ class UserService
     }
     
     public static function getWxId(){
-        $wxId = $_SESSION['ast_c_id_session_id'];
-        LoggerUtil::log("getWxId", "load wxid from session with value : " . $wxId);
+        $sessionId = $_SESSION['ast_c_id_session_id'];
+        $login_info = WeixinService::loadLoginInfo();
+        
+        if(!empty($login_info)){
+            LoggerUtil::log("getWxId", "load wxid from session " . $sessionId . " and open id is : " . $login_info->openId);
+        }
+        
         if(empty($wxId) && ($_SERVER['HTTP_HOST']=='127.0.0.1' || $_SERVER['HTTP_HOST']=='localhost')){
             $wxId = uniqid();           
             setcookie("ast_c_id",$wxId,time()+7*24*3600,"/");
