41b75fd0ce6fc70af2c9572c43c31cc0a42084c3
dylan <dylan@test.com>
2017-09-13 23:11:57 +0800

    php error

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,7 +10,6 @@ use Drupal\astrology\Data\DataUtil;
 use Drupal\astrology\service\UserService;
 use Drupal\astrology\service\WeixinService;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Drupal\astrology\service\WeixinService;
 
 /**
  * QuestionController
