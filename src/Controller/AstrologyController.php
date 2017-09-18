f58edacd4f3810a60eb69836b1cdd0b514c3d54b
dylan <dylan@test.com>
2017-09-19 00:10:46 +0800

    fix bug

--- src/Controller/AstrologyController.php
+++ src/Controller/AstrologyController.php
@@ -126,7 +126,7 @@ class AstrologyController extends ControllerBase
             $join_type = 'gerenxingpan_caifumima';
         }
         
-        $query_sql = "select t.title, b.body_value from  node_field_data as t, node__body as b, node as n where n.type = '" . $join_type . "' and n.nid=t.nid and n.nid=b.entity_id";
+        $query_sql = "select t.nid, t.title, b.body_value from  node_field_data as t, node__body as b, node as n where n.type = '" . $join_type . "' and n.nid=t.nid and n.nid=b.entity_id";
         $query_results = \Drupal::database()->query($query_sql)->fetchAll();
         
         $final_query_results = array();
